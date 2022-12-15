ce38df0a9fcef34ceb74846964cffb72f44540b9
ZavClub <76707395+ZavClub@users.noreply.github.com>
2022-12-16 00:58:31 +0300

    Выполнено 4 задание

---
+++ Seminar-1/Seminar-1_DZ-4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Clear();
+
+Console.WriteLine("Вывод чётных чисел, до введённого");
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int cur = 2;
+
+if (cur > num) Console.Write("Вы ввели неверное число");
+
+while (cur <= num)
+{
+    Console.Write($"{cur}");
+    cur += 2;
+    
+    if (cur <= num) Console.Write(", ");   
+}    
