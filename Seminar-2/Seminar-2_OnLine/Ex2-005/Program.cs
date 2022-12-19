8ffcd73e98cdf47defbe04c604683552b6aa622e
ZavClub <76707395+ZavClub@users.noreply.github.com>
2022-12-19 21:48:54 +0300

    Создание папок DZ

--- Seminar-2/Seminar-2_OnLine/Ex2-005/Program.cs
+++ Seminar-2/Seminar-2_OnLine/Ex2-005/Program.cs
@@ -36,11 +36,12 @@ int randNum = new Random().Next(10, 100);
 int newNumber = CutNumber(randNum);
 Console.WriteLine($"Сгенерированное число: {randNum} Наибольшее число: {newNumber}"); */
 
-//Задача 2. Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому.
+// Задача 2. Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому.
 
 bool kratNum(int num1, int num2)
 {
-    if (num2 % num1 == 0) return true;
+    if (num2 % num1 == 0)
+        return true;
     return false;
 }
 
@@ -55,7 +56,7 @@ if (kratNum(number1, number2) == true)
     Console.WriteLine("Да, является");
 else
     Console.WriteLine("Нет, не является");
-
+    
 //Console.WriteLine(KratNum(number1, number2));
 
 //Задача 3. Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
@@ -67,6 +68,8 @@ else
     else
         return false;
 }
+
+
 
 Console.Clear();
 Console.WriteLine("Проверка, является ли одно число квадратом другого");
