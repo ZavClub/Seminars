98711555c562e7aeda4d11e2a81587a6fcd333eb
ZavClub <76707395+ZavClub@users.noreply.github.com>
2023-01-31 00:13:56 +0300

    DZ-009

---
+++ Seminar-9/Seminar-9-OnLine/Ex9-001/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте значения m и n. Напишите программу, которая выводит все натуральные числа в промежутке от m до n.
+
+void ShowNum(int m, int n)
+{
+    // if(m > n)
+    // {
+    //     Console.Write(m + " ");
+    //     ShowNum(m - 1, n);
+    // }    
+    // else if(n > m)
+    // {
+    //     ShowNum(m, n - 1);
+    //     Console.Write(n + " ");
+    // }
+    // else Console.Write(n + " ");
+
+    Console.Write(m + " ");
+    if (m < n) ShowNum(m + 1, n);
+    if (m > n) ShowNum(m - 1, n);
+}
+
+Console.WriteLine();
+ShowNum(10, 15);
---
+++ Seminar-9/Seminar-9-OnLine/Ex9-002/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая на вход принимает два числа A и B и возводит число A в целую степень B (Положительное и отрицательное)
+
+double AtoBdegree(double a, int b)
+{
+    if(b > 0) return AtoBdegree(a, b - 1) * a;
+    else if(b < 0) return AtoBdegree(a, b + 1) * (1 / a);
+    else return 1;
+}
+
+Console.WriteLine(AtoBdegree(2, 3));
