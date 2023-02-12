dde0813d1d116f7b1d5d15a832f3ddebccf34c16
ZavClub <76707395+ZavClub@users.noreply.github.com>
2023-02-13 00:39:52 +0300

    Dz-Четверть 1

---
+++ Final_Test/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string[] array = new string[size];
+
+for (int i = 0; i < size; i++)
+{
+    Console.Write("Введите данные в массив: ");
+    string? username = Console.ReadLine();
+    array[i] = username.ToString();
+}
+int tmp = 0;
+void FindLiterals()
+{
+
+    for (int index = 0; index < size; index++)
+    {
+        if (index == 0) Console.Write("[");
+
+        int num = 0;
+
+        foreach (char c in array[index]) num++;
+
+        if (num > 0 && num <= 3)
+        {
+            tmp++;
+            if (tmp > 1 && (num > 0 && num <= 3)) Console.Write(",");
+            Console.Write($"'{array[index]}'");
+        }
+        if (index == size - 1) Console.Write("]");
+    }
+}
+
+FindLiterals();
